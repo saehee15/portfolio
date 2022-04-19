cfa072bcb36dcf782a6e3dfa31937228d54d424e
saehee15 <sae_hwi@naver.com>
2022-04-19 22:44:18 +0900

    20200419_로딩화면 테스트

--- js/main.js
+++ js/main.js
@@ -1,50 +1,79 @@
-$(function(){
+$(function () {
 
-    $('.click').click(function (e) { 
+    const loader = $('.loading');
+    const html = $('html');
+
+    html.css({
+        'overflow': 'hidden'
+    }); //로딩 중 스크롤 방지
+
+        setTimeout(() => {
+            loader.fadeOut(300);
+            html.css({
+                'overflow': 'auto'
+            }); //스크롤 방지 해제
+
+        }, 5000);
+
+
+    $('.click').click(function (e) {
         e.preventDefault();
         target = $('.work').offset().top
 
-        gsap.to('body, html',{
-            'scrollTop' : target
+        gsap.to('body, html', {
+            'scrollTop': target
         })
-        
+
     });
 
-    
-    ScrollTrigger.create({
-            trigger:".work",
-            start:"top 0%",
-            markers: true,
-            onEnter:function(){
-                $('.up_btn').addClass('on');
-            },
-            onLeaveBack:function(){
-                $('.up_btn').removeClass('on')
-            }
-     })
 
-    $('.up_btn').click(function (e) { 
-            e.preventDefault();
-            target = $('.about').offset().top
-    
-            gsap.to('body, html',{
-                'scrollTop' : target
-            })
-            
-     });
+    ScrollTrigger.create({
+        trigger: ".work",
+        start: "top 0%",
+        markers: true,
+        onEnter: function () {
+            $('.up_btn').addClass('on');
+        },
+        onLeaveBack: function () {
+            $('.up_btn').removeClass('on')
+        }
+    })
+
+    $('.up_btn').click(function (e) {
+        e.preventDefault();
+        target = $('.about').offset().top
+
+        gsap.to('body, html', {
+            'scrollTop': target
+        })
+
+    });
 
 
     //txt
     const prdMotion = gsap.timeline({
-        defaults:{
-            duration:1,
+        defaults: {
+            duration: 1,
         }
     })
-    const prdTxt = new SplitType('.left .txt_hide h2', { types: 'words, chars', });
+    const prdTxt = new SplitType('.left .txt_hide h2', {
+        types: 'words, chars',
+    });
 
     prdMotion.addLabel('m1')
-    .fromTo(prdTxt.chars,{opacity:0, yPercent: 1},{opacity:1,yPercent: 0,stagger:0.05},'m1')
-    .fromTo('.txt_hide li',{opacity:0},{opacity:1},"m1+=0.5")
+        .fromTo(prdTxt.chars, {
+            opacity: 0,
+            yPercent: 1
+        }, {
+            opacity: 1,
+            yPercent: 0,
+            stagger: 0.05
+        }, 'm1')
+        .fromTo('.txt_hide li', {
+            opacity: 0
+        }, {
+            opacity: 1
+        }, "m1+=0.5")
     // .fromTo('.prd_item .img_container',{opacity:0,rotation:3},{opacity:1, rotation:0},"m1+=1")
 
 
@@ -59,5 +88,5 @@ $(function(){
     //     stagger:0.05
     // })
 
-    
+
 })
