feba7a1e6fde62d6794648e3a29b1044de68e4ad
saehee15 <sae_hwi@naver.com>
2022-04-23 23:18:09 +0900

    20220423_디자인 변경

--- js/main.js
+++ js/main.js
@@ -44,34 +44,19 @@ $(function () {
 
     
 
-    // about
-    $('.m_about').click(function (e) {
+    //about
+    $('.link-target').click(function(e){
         e.preventDefault();
-        target = $('.about').offset().top
+
+        var target= $(this).data('target');
+        var targetOffset = $(target).offset().top;
 
         gsap.to('body, html', {
-            'scrollTop': target
-        })
+                    'scrollTop': targetOffset
+                })
 
-    });
-    $('.m_work').click(function (e) {
-        e.preventDefault();
-        target = $('.work').offset().top
+    })
 
-        gsap.to('body, html', {
-            'scrollTop': target
-        })
-
-    });
-    $('.m_contact').click(function (e) {
-        e.preventDefault();
-        target = $('footer').offset().top
-
-        gsap.to('body, html', {
-            'scrollTop': target
-        })
-
-    });
 
     $('.click').click(function (e) {
         e.preventDefault();
@@ -106,79 +91,146 @@ $(function () {
 
     });
 
+   txtMotion = gsap.timeline({
+    scrollTrigger:{
+        trigger:".about",
+        start: "top top",
+        end:"bottom top",
+        scrub:1,
+        // markers: true,
+        pin: true,
+
+    }
+    })
+
+
+    txtMotion.set('.word',{
+        yPercent:  300, 
+        scale: 2
+    })
+    .to('.word',{
+        yPercent:0,
+        opacity: 1,
+        scale:1,
+        stagger:0.2
+    })
+
+
 
     //txt
-    const prdMotion = gsap.timeline({
-        defaults: {
-            duration: 5,
-        }
-    })
-    const prdTxt = new SplitType('.left .txt_hide h2', {
-        types: 'words, chars',
-    });
+    // const prdMotion = gsap.timeline({
+    //     defaults: {
+    //         duration: 5,
+    //     }
+    // })
+    // const prdTxt = new SplitType('.left .txt_hide h2', {
+    //     types: 'words, chars',
+    // });
 
-    prdMotion.addLabel('m1')
-        .fromTo(prdTxt.chars, {
-            opacity: 0,
-            yPercent: 1
-        }, {
-            opacity: 1,
-            yPercent: 0,
-            stagger: 0.05
-        }, 'm1')
-        .fromTo('.txt_hide li',{
-            opacity: 0
-        }, {
-            opacity: 1
-        }, "m1+=0.5")
+    // prdMotion.addLabel('m1')
+    //     .fromTo(prdTxt.chars, {
+    //         opacity: 0,
+    //         yPercent: 1
+    //     }, {
+    //         opacity: 1,
+    //         yPercent: 0,
+    //         stagger: 0.05
+    //     }, 'm1')
+    //     .fromTo('.txt_hide li',{
+    //         opacity: 0
+    //     }, {
+    //         opacity: 1
+    //     }, "m1+=0.5")
   
 
-    //work
-    gsap.utils.toArray('.work .card').forEach(el => {
-        gsap.fromTo(el,{
-            scale:0.8,
-            opacity:0,
-            filter: 'blur(30px)'
-        },{
-            scrollTrigger:{
-                trigger:el,
-                start:"top 50%",
-                markers: true
-            },
-            scale:1,
-            opacity: 1,
-            filter: 'blur(0px)'
-        })
-    });
-
-
    
+    // gsap.fromTo(el,{
+    //     opacity:0,
+    //     filter: 'blur(30px)'
+    // },{
+    //     scrollTrigger:{
+    //         trigger:el,
+    //         start:"top 50%",
+    //         // markers: true
+    //     },
+    //     opacity: 1,
+    //     filter: 'blur(0px)'
+    // })
+
+    //work
+    gsap.set('.work .card',{opacity: 0,filter: 'blur(30px)'})
+    gsap.utils.toArray('.work .card').forEach(el => {
+
+        ScrollTrigger.create({
+            trigger:el,
+            start: "top 80%",
+            end: "bottom 30%",
+            // markers: true,
+            onEnter:()=>{
+                gsap.to(el,{opacity:1,filter: 'blur(0px)'})
+            },
+            onLeave:()=>{
+                gsap.to(el,{opacity:0,filter: 'blur(30px)'})
+            },
+            onEnterBack:()=>{
+                gsap.to(el,{opacity:1,filter: 'blur(0px)'})
+            },
+            onLeaveBack:()=>{
+                gsap.to(el,{opacity:0,filter: 'blur(30px)'})
+            },
+
+        })
+       
+    });
 
     var path = document.querySelector('#line');
-    // Get length of path...
     var pathLength = path.getTotalLength();
+
+    gsap.set('#line',{
+        strokeDasharray: pathLength + ' ' + pathLength,
+        strokeDashoffset: pathLength,
+    })
+
+
+    gsap.to('#line',5,{
+        scrollTrigger:{
+            trigger:'.work',
+            start:"top 70%",
+            // end:"+=300%",
+            end:"bottom bottom",
+            scrub:1,
+            // markers:true,
+        },
+        strokeDasharray: pathLength + pathLength,
+        ease:'none',
+        // strokeDashoffset: pathLength,
+    })
+
+    // var path = document.querySelector('#line');
+    // // Get length of path...
+    // var pathLength = path.getTotalLength();
     
-    console.log(pathLength);
+    // console.log(pathLength);
     
-    // Make very long dashes (the length of the path itself)
-    path.style.strokeDasharray = pathLength + ' ' + pathLength ;
+    // // Make very long dashes (the length of the path itself)
+    // path.style.strokeDasharray = pathLength + ' ' + pathLength ;
     
-    // Offset the dashes so the it appears hidden entirely
-    path.style.strokeDashoffset = pathLength;
+    // // Offset the dashes so the it appears hidden entirely
+    // path.style.strokeDashoffset = pathLength;
     
-    // When the page scrolls...
-    window.addEventListener("scroll", function(e) {
+    // // When the page scrolls...
+    // window.addEventListener("scroll", function(e) {
      
-      // What % down is it? 
-      var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    //   // What % down is it? 
+    //   var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
         
-      // Length to offset the dashes
-      var drawLength = pathLength  * scrollPercentage;
+    //   // Length to offset the dashes
+    //   var drawLength = pathLength  * scrollPercentage;
       
-      // Draw in reverse
-      path.style.strokeDashoffset = pathLength - drawLength;
+    //   // Draw in reverse
+    //   path.style.strokeDashoffset = pathLength - drawLength;
       
-    });
+    // });
 
 
 })
